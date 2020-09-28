b5f6e9547a37e13c7f745571f2060ee7a07947b2
Marcos Oliveira Pinto <marcos.oliveira@wevo.com.br>
2020-09-28 20:57:11 -0300

    Incluindo implementação de JWT na controller de autenticação para que o token seja retornando no momento do login e do cadastro do usuário

--- src/Services/MksStoreEnterprise.Identity.API/Controllers/AuthController.cs
+++ src/Services/MksStoreEnterprise.Identity.API/Controllers/AuthController.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MksStoreEnterprise.Identity.API.Extensions;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using static MksStoreEnterprise.Identity.API.Models.UserViewModels;
 
@@ -12,12 +21,14 @@ namespace MksStoreEnterprise.Identity.API.Controllers
         //Global
         private readonly SignInManager<IdentityUser> __signinManager;
         private readonly UserManager<IdentityUser> __userManager;
+        private readonly AppSettings __appSettings;
 
         //Constructor
-        public AuthController(SignInManager<IdentityUser> signinManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signinManager, UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings)
         {
             __signinManager = signinManager;
             __userManager = userManager;
+            __appSettings = appSettings.Value;
         }
 
         //Methods
@@ -40,7 +51,9 @@ namespace MksStoreEnterprise.Identity.API.Controllers
             if (result.Succeeded)
             {
                 await __signinManager.SignInAsync(user, false);
-                return Ok();
+                return Ok(
+                     await this.GenerateJWT(_userRegistry.email)
+                );
             }
 
             return BadRequest();
@@ -57,10 +70,67 @@ namespace MksStoreEnterprise.Identity.API.Controllers
             var result = await __signinManager.PasswordSignInAsync(_userLogin.email, _userLogin.password, false, true);
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(
+                    await this.GenerateJWT(_userLogin.email)
+                );
             }
 
             return BadRequest();
         }
+
+        [NonAction]
+        public async Task<UserResponseLogin> GenerateJWT(string _email)
+        {
+            IdentityUser user = await __userManager.FindByEmailAsync(_email);
+            IList<Claim> claims = await __userManager.GetClaimsAsync(user);
+            IList<string> roles = await __userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (string userRole in roles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            ClaimsIdentity identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(__appSettings.Secret);
+
+            SecurityToken token = tokenHandler.CreateToken(new SecurityTokenDescriptor { 
+                Issuer = __appSettings.Emitter,
+                Audience = __appSettings.ValidAt,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(__appSettings.ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            string encodedToken = tokenHandler.WriteToken(token);
+            UserResponseLogin response = new UserResponseLogin()
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(__appSettings.ExpirationHours).TotalSeconds,
+                UserToken = new UserToken()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(w => new UserClaim {
+                        Type = w.Type,
+                        Value = w.Value
+                    })
+                }
+            };
+
+            return response;
+        }
+
+        [NonAction]
+        private static long ToUnixEpochDate(DateTime _date)
+        => (long)Math.Round((_date.ToUniversalTime() - new DateTimeOffset(170, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalMilliseconds);
     }
 }
--- src/Services/MksStoreEnterprise.Identity.API/Models/UserViewModels.cs
+++ src/Services/MksStoreEnterprise.Identity.API/Models/UserViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MksStoreEnterprise.Identity.API.Models
 {
@@ -29,6 +30,26 @@ namespace MksStoreEnterprise.Identity.API.Models
             [Required(ErrorMessage = "O campo {0} é obrigatório")]
             [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
             public string password { get; set; }
+        }
+
+        public class UserResponseLogin
+        {
+            public string AccessToken { get; set; }
+            public double ExpiresIn { get; set; }
+            public UserToken UserToken { get; set; }
+        }
+
+        public class UserToken
+        {
+            public string Id { get; set; }
+            public string Email { get; set; }
+            public IEnumerable<UserClaim> Claims { get; set; }
+        }
+
+        public class UserClaim
+        {
+            public string Value { get; set; }
+            public string Type { get; set; }
         }
     }
 }
